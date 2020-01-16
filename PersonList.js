b00766c90e545bbd7732fb6bf6242a0c54b7cf7e
mnsmanasa <saimanasa1502@gmail.com>
2020-01-16 12:33:07 +0530

    1 file modified

--- PersonList.js
+++ PersonList.js
@@ -1,34 +1,41 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 export default class PersonList extends React.Component {
   state = {
     persons: [],
-    id:0
-  }
+    id: 0
+  };
 
   componentDidMount() {
-    axios.get(`https://jsonplaceholder.typicode.com/users`)
-      .then(res => {
-        const persons = res.data;
-        this.setState({ persons });
-      })
+    axios.get(`https://jsonplaceholder.typicode.com/users`).then(res => {
+      const persons = res.data;
+      this.setState({ persons });
+    });
   }
 
-  delete=(event)=> {
-    axios.delete(`https://jsonplaceholder.typicode.com/users/${event.target.id}`)
+  deletePerson = (event) => {
+    var id = event.target.id;
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/users/${id}`)
       .then(res => {
-        const persons = res.data;
-        console.log(res.data)
-      })
-
-  }
+        this.setState({
+          persons: this.state.persons.filter(person => {
+            return person.id !== Number(id);
+          })
+        });
+      });
+  };
 
   render() {
     return (
       <ul>
-        { this.state.persons.map(person => <li id={person.id} onClick={this.delete} key={person.id}>{person.name}</li>)}
+        {this.state.persons.map(person => (
+          <li id={person.id} onClick={this.deletePerson} key={person.id}>
+            {person.name}
+          </li>
+        ))}
       </ul>
-    )
+    );
   }
-} 
+}
