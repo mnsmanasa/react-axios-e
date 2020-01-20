cc5963a969ab183f422c484fbd22124e9bfeae1a
mnsmanasa <saimanasa1502@gmail.com>
2020-01-20 15:27:06 +0530

    1 file modified

--- PersonList.js
+++ PersonList.js
@@ -2,47 +2,27 @@ import React from "react";
 import axios from "axios";
 
 class PersonList extends React.Component {
-  state = {
-    persons: [],
-    id: 0
-  };
+  constructor() {
+    super();
+    this.state = {
+      persons: [],
+      id: 0
+    };
+  }
 
   componentDidMount() {
-    axios.get(`https://jsonplaceholder.typicode.com/users`).then(res => {
-      const persons = res.data;
-      this.setState({ persons });
-    });
   }
 
   deletePerson = event => {
-    var id = event.target.id;
-    axios
-      .delete(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then(res => {
-        this.setState({
-          persons: this.state.persons.filter(person => {
-            return person.id !== Number(id);
-          })
-        });
-      });
+   
   };
 
   render() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`).then(res => {
+      console.log(res.data)
+    });
     return (
-      <ul>
-        {this.state.persons.map(person => {
-          <li key={person.id}>
-            {person.name}
-            <span
-              className="delete-btn"
-              id={person.id}
-              onClick={this.deletePerson}
-            >
-              X
-            </span>
-          </li>;
-        })}
-      </ul>
+      <div>jgj</div>
     );
   }
 }
